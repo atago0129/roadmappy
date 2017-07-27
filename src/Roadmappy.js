772900aec54884d363279688765b8c32bbbd63ef
atago0129 <atsushi.tago@gmail.com>
2017-07-27 12:12:27 +0900

    [WIP] port roadmap.js with d3 v4

--- src/Roadmappy.js
+++ src/Roadmappy.js
@@ -68,6 +68,7 @@ export class Roadmappy {
 
   render(targetElementName) {
     this._draw(d3.select("#" + targetElementName), this.parsedItems.tasks);
+    this._draw(d3.select("#" + targetElementName), this.parsedItems.people);
   }
 
   _draw(targetElement, items) {
@@ -76,7 +77,7 @@ export class Roadmappy {
     const topPadding = 20;
 
     const h = items.length * gap + 40;
-    const w = targetElement.clientWidth;
+    const w = targetElement.node().clientWidth;
 
     let svg = targetElement.append("svg").attr("width", w).attr("style", "overflow: visible");
     svg.attr("height", function() {
@@ -169,7 +170,7 @@ export class Roadmappy {
       .attr("text-height", 14)
       .attr("fill", "#000");
 
-    let sidePadding = 15;
+    let sidePadding = axisText.node().parentNode.getBBox().width + 15;
 
     // Init time scale
     let xScale = d3.scaleTime()
@@ -181,58 +182,161 @@ export class Roadmappy {
           return d.to;
         })
       ])
-      .range([0, w - sidePadding - 15]);
+      .rangeRound([0, w - sidePadding - 15]);
 
     let xAxis = d3.axisBottom(xScale)
-      .ticks(d3.timeMonday);
-    svg.append("g")
-      .attr("transform", "translate(0," + svg.attr("height") + ")")
+      .ticks(d3.timeMonday)
+      .tickSize(- svg.attr("height") + topPadding + 20, 0, 0)
+      .tickFormat(d3.timeFormat("%b %d"));
+    let xAxisGroup = svg.append("g")
+      .attr("transform", "translate(" + sidePadding + "," + (svg.attr("height") -20) + ")")
       .call(xAxis);
 
-    // // Init X Axis
-    // var xAxis = d3.svg.axis()
-    //   .scale(timeScale)
-    //   .orient("bottom")
-    //   .ticks(d3.time.monday)
-    //   .tickSize(- svg.attr("height") + topPadding + 20, 0, 0)
-    //   .tickFormat(d3.time.format(currentInstance.graphOption.dateFormat ? currentInstance.graphOption.dateFormat : "%b %d"));
-    // if (currentInstance.graphOption.ticksType === "month") {
-    //   xAxis.ticks(d3.time.month);
-    // }
-    //
-    // // Draw vertical grid
-    // var xAxisGroup = svg.append("g")
-    //   .attr("transform", "translate(" + sidePadding + ", " + (svg.attr("height") - 20) + ")")
-    //   .call(xAxis);
+    xAxisGroup.selectAll("text")
+      .style("text-anchor", "middle")
+      .attr("fill", "#000")
+      .attr("stroke", "none")
+      .attr("font-size", 10)
+      .attr("dy", "1em");
 
-    // xAxisGroup.selectAll("text")
-    //   .style("text-anchor", "middle")
-    //   .attr("fill", "#000")
-    //   .attr("stroke", "none")
-    //   .attr("font-size", 10)
-    //   .attr("dy", "1em");
-    //
-    // xAxisGroup.selectAll(".tick line")
-    //   .attr("stroke", "#dddddd")
-    //   .attr("shape-rendering", "crispEdges");
-    //
-    // // Now
-    // let now = new Date();
-    // if (now > timeScale.domain()[0] && now < timeScale.domain()[1]) {
-    //   xAxisGroup
-    //     .append("line")
-    //     .attr("x1", timeScale(now))
-    //     .attr("y1", 0)
-    //     .attr("x2", timeScale(now))
-    //     .attr("y2", -svg.attr("height") + topPadding + 20)
-    //     .attr("class", "now");
-    //
-    //   xAxisGroup.selectAll(".now")
-    //     .attr("stroke", "red")
-    //     .attr("opacity", 0.5)
-    //     .attr("stroke-dasharray", "2,2")
-    //     .attr("shape-rendering", "crispEdges");
-    // }
+    xAxisGroup.selectAll(".tick line")
+      .attr("stroke", "#dddddd")
+      .attr("shape-rendering", "crispEdges");
+
+    // Now
+    let now = new Date();
+    if (now > xScale.domain()[0] && now < xScale.domain()[1]) {
+      xAxisGroup
+        .append("line")
+        .attr("x1", xScale(now))
+        .attr("y1", 0)
+        .attr("x2", xScale(now))
+        .attr("y2", -svg.attr("height") + topPadding + 20)
+        .attr("class", "now");
+
+      xAxisGroup.selectAll(".now")
+        .attr("stroke", "red")
+        .attr("opacity", 0.5)
+        .attr("stroke-dasharray", "2,2")
+        .attr("shape-rendering", "crispEdges");
+    }
+
+    // Items group
+    let rectangles = svg.append("g")
+      .attr("transform", "translate(" + sidePadding + ", 0)")
+      .selectAll("rect")
+      .data(items)
+      .enter();
+
+    // Draw items boxes
+    rectangles.append("rect")
+      .attr("rx", 3)
+      .attr("ry", 3)
+      .attr("x", function(d){
+        return xScale(d.from);
+      })
+      .attr("y", function(d, i){
+        return i * gap + topPadding;
+      })
+      .attr("width", function(d){
+        return xScale(d.to) - xScale(d.from);
+      })
+      .attr("height", barHeight)
+      .attr("stroke", "none")
+      .attr("fill", function(d) {
+        return d.pattern || d.color;
+      })
+      .attr("fill-opacity", 0.5)
+      .on("mouseover", function() {
+        d3.select(this).style({cursor:"pointer"});
+      });
+
+    // Draw items texts
+    rectangles.append("text")
+      .text(function(d){
+        return d.name;
+      })
+      .attr("x", function(d){
+        return xScale(d.from) + (xScale(d.to) - xScale(d.from)) / 2;
+      })
+      .attr("y", function(d, i){
+        return i * gap + 14 + topPadding;
+      })
+      .attr("font-size", 11)
+      .attr("font-weight", function(d) {
+        return d.style;
+      })
+      .attr("text-anchor", "middle")
+      .attr("text-height", barHeight)
+      .attr("fill", "#000")
+      .style("pointer-events", "none");
+
+    // Draw vertical mouse helper
+    let verticalMouse = svg.append("line")
+      .attr("x1", 0)
+      .attr("y1", 0)
+      .attr("x2", 0)
+      .attr("y2", 0)
+      .style("stroke", "black")
+      .style("stroke-width", "1px")
+      .style("stroke-dasharray", "2,2")
+      .style("shape-rendering", "crispEdges")
+      .style("pointer-events", "none")
+      .style("display", "none");
+
+    let verticalMouseBox = svg.append("rect")
+      .attr("rx", 3)
+      .attr("ry", 3)
+      .attr("width", 50)
+      .attr("height", barHeight)
+      .attr("stroke", "none")
+      .attr("fill", "black")
+      .attr("fill-opacity", 0.8)
+      .style("display", "none");
+
+    let verticalMouseText = svg.append("text")
+      .attr("font-size", 11)
+      .attr("font-weight", "bold")
+      .attr("text-anchor", "middle")
+      .attr("text-height", barHeight)
+      .attr("fill", "white")
+      .style("display", "none");
+
+    let verticalMouseTopPadding = 40;
+
+    svg.on("mousemove", function () {
+      let xCoord = d3.mouse(this)[0],
+        yCoord = d3.mouse(this)[1];
+
+      if (xCoord > sidePadding) {
+        verticalMouse
+          .attr("x1", xCoord)
+          .attr("y1", 10)
+          .attr("x2", xCoord)
+          .attr("y2", svg.attr("height") - 20)
+          .style("display", "block");
+
+        verticalMouseBox
+          .attr("x", xCoord - 25)
+          .attr("y", yCoord - (barHeight + 8) / 2 + verticalMouseTopPadding)
+          .style("display", "block");
+
+        verticalMouseText
+          .attr("transform", "translate(" + xCoord + "," + (yCoord + verticalMouseTopPadding) + ")")
+          .text(d3.timeFormat("%b %d")(xScale.invert(xCoord - sidePadding)))
+          .style("display", "block");
+      } else {
+        verticalMouse.style("display", "none");
+        verticalMouseBox.style("display", "none");
+        verticalMouseText.style("display", "none");
+      }
+    });
+
+    svg.on("mouseleave", function() {
+      verticalMouse.style("display", "none");
+      verticalMouseBox.style("display", "none");
+      verticalMouseText.style("display", "none");
+    });
 
   }
 
