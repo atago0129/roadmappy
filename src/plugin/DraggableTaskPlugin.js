86e57f843612d3ab19e7f8b39356d9f565c61824
atago0129 <atsushi.tago@gmail.com>
2017-08-31 18:22:46 +0900

    reflect review comment

--- src/plugin/DraggableTaskPlugin.js
+++ src/plugin/DraggableTaskPlugin.js
@@ -1,7 +1,10 @@
 import {PluginInterface} from "./PluginInterface";
 
+const DRAG_THRESHOLD_PIXEL = 10;
+
 export class DraggableTaskPlugin extends PluginInterface {
 
+  isDragging = false;
   dragStartPos = null;
 
   /**
@@ -10,36 +13,49 @@ export class DraggableTaskPlugin extends PluginInterface {
   initialize(roadmappy) {
     this.roadmappy = roadmappy;
 
-    roadmappy.on('drag:drag:task:from', this._drag.bind(this, 'from', false));
-    roadmappy.on('drag:end:task:from', this._drag.bind(this, 'from', true));
-    roadmappy.on('drag:drag:task:to', this._drag.bind(this, 'to', false));
-    roadmappy.on('drag:end:task:to', this._drag.bind(this, 'to', true));
+    roadmappy.on('drag:start:task:from', this._startDrag.bind(this));
+    roadmappy.on('drag:drag:task:from', this._draggingEdgeOfTask.bind(this, 'from'));
+    roadmappy.on('drag:end:task:from', this._endDrag.bind(this));
 
-    roadmappy.on('drag:drag:task', this._move.bind(this, false));
-    roadmappy.on('drag:end:task', this._move.bind(this, true));
+    roadmappy.on('drag:start:task:to', this._startDrag.bind(this));
+    roadmappy.on('drag:drag:task:to', this._draggingEdgeOfTask.bind(this, 'to'));
+    roadmappy.on('drag:end:task:to', this._endDrag.bind(this));
+
+    roadmappy.on('drag:start:task', this._startDrag.bind(this));
+    roadmappy.on('drag:drag:task', this._draggingTask.bind(this));
+    roadmappy.on('drag:end:task', this._endDrag.bind(this));
   }
 
-  _drag(key, reorder, task, pos) {
-    const xScale = this.roadmappy.canvas.xScale;
-    task[key] = xScale.invert(pos.x);
-    if (reorder) this.roadmappy.roadmap.reorder();
-    this.roadmappy.render();
+  _startDrag(task, pos) {
+    this.dragStartPos = pos;
   }
 
-  _move(isDragEnd, task, pos) {
-    if (this.dragStartPos === null) {
-      this.dragStartPos = pos;
-    } else if (Math.abs(this.dragStartPos.x - pos.x) > 10) {
+  _draggingTask(task, pos) {
+    if (this.isDragging) {
       const xScale = this.roadmappy.canvas.xScale;
       const width = xScale(task.to) - xScale(task.from);
       task.from = xScale.invert(pos.x - width / 2);
       task.to = xScale.invert(pos.x + width / 2);
-      if (isDragEnd) {
-        this.roadmappy.roadmap.reorder();
-        this.dragStartPos = null;
-      }
       this.roadmappy.render();
+    } else {
+      this.isDragging = Math.abs(this.dragStartPos.x - pos.x) > DRAG_THRESHOLD_PIXEL;
     }
+  }
+
+  _draggingEdgeOfTask(key, task, pos) {
+    if (this.isDragging) {
+      const xScale = this.roadmappy.canvas.xScale;
+      task[key] = xScale.invert(pos.x);
+      this.roadmappy.render();
+    } else {
+      this.isDragging = Math.abs(this.dragStartPos.x - pos.x) > DRAG_THRESHOLD_PIXEL;
+    }
+  }
+
+  _endDrag(task, pos) {
+    this.roadmappy.roadmap.reorder();
+    this.dragging = false;
+    this.dragStartPos = null;
   }
 
 }
